d2ef52ed9a9b7abd5de991c6e41b0b29c8a36d43
Bala <subramaniambala063@gmail.com>
2019-12-09 00:54:51 +0400

    Update view.component.ts

--- taskmanagerUI/src/app/task/view/view.component.ts
+++ taskmanagerUI/src/app/task/view/view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskmanagerService } from '../taskmanager.service';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  taskResponse: Array<any>;
+
+  constructor(private taskManagerService: TaskmanagerService) { }
 
   ngOnInit() {
+    this.taskManagerService.getAll().subscribe(data => {
+      this.taskResponse = data;
+    });
   }
 
 }
